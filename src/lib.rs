3a8356fc9a3b8a8c91786e802d2a3c9717977ad3
William Do <william@ultimaspin.com>
2019-05-18 11:56:53 +0100

    port the initial algorithm for drawing tournament tree from js

--- src/lib.rs
+++ src/lib.rs
@@ -36,29 +36,56 @@ pub fn drawSomething() {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
+    draw_tree(context, 4);
 
-    context.begin_path();
-
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
 }
+
+fn draw_tree(context: web_sys::CanvasRenderingContext2d, levels: u32) {
+
+    let player_box_height = 40.0;
+    let player_box_width = 120.0;
+    let player_box_gap = 20.0;
+
+    for i in 0..levels {
+        let number_nodes = 2u32.pow(levels - i - 1);
+        let offset = 2u32.pow(i) as f64 * (player_box_height + player_box_gap);
+        let horizontal_bias = (i + 1) as f64 * player_box_gap + i as f64 * player_box_width;
+
+        for node_idx in 0..number_nodes {                    
+            let vertical_bias = ((2u32.pow(i) - 1) as f64 * (player_box_gap + player_box_height)) / 2.0;
+            context.stroke_rect(
+                horizontal_bias,
+                node_idx as f64 * offset + vertical_bias,
+                player_box_width,
+                player_box_height
+            );
+        }
+
+    }
+
+}
+
+/*
+        function drawTree(ctx, levels) {
+            var playerBoxHeight = 40;
+            var playerBoxWidth = 120;
+            var playerBoxGap = 20;
+
+            for (var i = 0; i < levels; i++) {
+                var numberNodes = Math.pow(2, levels - i - 1);
+                var offset = Math.pow(2, i) * (playerBoxHeight + playerBoxGap);
+                var horizontalBias = (i + 1) * playerBoxGap + i * playerBoxWidth;
+
+                for (var nodeIdx = 0; nodeIdx < numberNodes; nodeIdx++) {                    
+                    var verticalBias = ((Math.pow(2, i) - 1) * (playerBoxGap + playerBoxHeight)) / 2;
+                    ctx.strokeRect(
+                        horizontalBias,
+                        nodeIdx * offset + verticalBias,
+                        playerBoxWidth,
+                        playerBoxHeight
+                    );
+                }
+
+            }
+        }
+*/
